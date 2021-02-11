14b9fc6b556146efa2f6c8f47485e8b7041034dc
Liem <liemeldert@outlook.com>
2021-02-11 11:43:08 -0500

    Added a few more initial files. YAML config added.

--- src/main/java/dev/joinaurora/Casino/Casino.java
+++ src/main/java/dev/joinaurora/Casino/Casino.java
@@ -1,4 +1,14 @@
 package dev.joinaurora.Casino;
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class Casino {
+public class Casino extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Aurora Casino plugin has loaded.");
+    }
+    @Override
+    public void onDisable() {
+
+    }
+
 }
